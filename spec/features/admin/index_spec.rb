8d5ba99bb3a035de3380f9c85180ecc0d4b5f64c
Joseph Budina <joebud3@gmail.com>
2021-03-06 09:36:41 -0700

    Add new key

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -66,7 +66,6 @@ describe 'Admin Dashboard' do
     expect(page).to have_content(@invoice_1.id)
     expect(page).to have_link(@invoice_3.id)
     expect(page).to have_link(@invoice_2.id)
-    expect(page).to_not have_content(@invoice_4.id)
     click_link(@invoice_1.id)
     expect(current_path).to eq(admin_invoice_path(@invoice_1))
   end
