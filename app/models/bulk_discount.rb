38c4981d2e67cf759062ad8ee5bd782d44c2ac57
Joseph Budina <joebud3@gmail.com>
2021-03-09 20:45:37 -0700

    Add styling

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,9 @@
 class BulkDiscount < ApplicationRecord
+  validates_presence_of :merchant_id,
+                        :percentage_discount,
+                        :threshold
+  validates_numericality_of :percentage_discount, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 100
+
   belongs_to :merchant
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
